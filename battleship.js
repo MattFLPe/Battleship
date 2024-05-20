c9a9d63212ee7f0fede0b59f8ffb3306527a5ef2
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-20 03:21:01 -0300

    Create a Ship class with two methods: hit() increase the number of hits in the ship isSunk() calculates whether a ship is considered sunk based on its length and the number of hits it has received

--- battleship.js
+++ battleship.js
@@ -1,3 +1,20 @@
 class Ship {
-    
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        this.hits++;
+    }
+
+    isSunk() {
+        if (this.hits >= this.length) {
+            this.sunk = true;
+            console.log("Ship has been sunk!");
+    } 
 }
+}
+
+module.exports = Ship;
