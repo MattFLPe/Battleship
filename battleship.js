bc66d34345177df5b4b01408d96292e33a0332c1
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-28 20:01:41 -0300

    Implement property to the Gameboard class: Stores missed attacks to be marked on a grid

--- battleship.js
+++ battleship.js
@@ -14,6 +14,7 @@ class Ship {
             this.sunk = true;
             console.log("Ship has been sunk!");
     } 
+    return this.hits === this.length;
 }
 }
 
@@ -22,6 +23,16 @@ class Gameboard {
         this.size = size;
         this.grid = this.createGrid(size);
         this.ships = [];
+        this.missedAttacks = this.createGrid(size)
+    }
+
+    allShipsSunk() {
+        for(const shipPlacement of this.ships) {
+            if (!shipPlacement.ship.isSunk()) {
+                return false; // If any ship is not sunk, return false
+            }
+        }
+        return true; // All ships are sunk
     }
 
     createGrid(size) {
@@ -30,11 +41,11 @@ class Gameboard {
             const row = [];
             for (let j = 0; j < size; j++) {
                 row.push(null); // Null represents empty space
-            }
+            };
             grid.push(row);
-        }
+        };
         return grid;
-    }
+    };
 
     placeShip(ship, x, y, orientation) {
         // Check if ship fits on the board
@@ -62,17 +73,31 @@ class Gameboard {
 
     receiveAttack(x, y) {
         // Process the attack
-        // For simplicity, let's assume a hit if there's a ship at the specified location
+        // assuming a hit if there's a ship at the specified location
         if (this.grid[y][x] !== null) {
             // Hit
             const ship = this.grid[y][x];
             ship.hit();
             return 'hit';
         } else {
+            // Mark the missed attack on the missedAttacks grid
+            this.missedAttacks[y][x] = 'miss';
             // Miss
             return 'miss';
         }
     }
 };
 
-module.exports = Ship, Gameboard;
+class Player {
+    constructor(player, computer) {
+        this.player = player;
+        this.computer = computer;
+    }
+}
+
+const gameboard = new Gameboard(10); // Create a gameboard with size 10x10
+const ship = new Ship(3); // Create a ship with length 3 (you need to define the Ship class)
+gameboard.placeShip(ship, 2, 3, 'horizontal'); // Place the ship at coordinates (2, 3) horizontally
+gameboard.receiveAttack(2, 3); // Attack the coordinates (2, 3)
+
+module.exports = { Ship, Gameboard };
