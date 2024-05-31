dc84e7dddd7c9e4e9b65e0e6e46907884c61d041
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-31 07:03:49 -0300

    Implement render function: Renders player's board and computer's board

--- domManager.js
+++ domManager.js
@@ -1,5 +1,31 @@
-const domManager = {
+import { Gameboard } from "./battleship.js";
+import { playerAttack } from "./main.js";
 
-};
 
-module.exports = domManager;
+
+export function render(gameboard, containerId) {
+    gameboard = new Gameboard();
+    
+    const container = document.getElementById('player-board');
+    const containerOpponent = document.getElementById('computer-board')
+
+    container.innerHTML = '';
+    containerOpponent.innerHTML = '';
+
+    // Loop through each cell in the gameboard
+    for (let y = 0; y < gameboard.grid.length; y++) {
+        for (let x = 0; x < gameboard.grid[y].length; x++) {
+            const cell = document.createElement('div');
+            const cellOpponent = document.createElement('div');
+            cell.classList.add('cell');
+            cellOpponent.classList.add('cellOpponent');
+            container.appendChild(cell);
+            containerOpponent.appendChild(cellOpponent);
+
+        }
+    }
+    containerOpponent.addEventListener('click', playerAttack);
+}
+
+
+
