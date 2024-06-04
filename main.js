ec07c56ffeaabb021c9f7e81195388fc0ec18419
Mateus Felipe <brasil.mateus748@gmail.com>
2024-06-04 10:52:44 -0300

    Implement function to initialize ships randomly

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 import { Ship, Gameboard, Player } from './battleship.js';
-import { startNewGame, handlePlayerAttack } from './gameLogic.js';
+//import { startGame } from './gameLogic.js';
+import { render } from './domManager.js'
+import { handleAttack } from './gameLogic.js';
+export { player1, player2 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    startNewGame();
-});
-
-document.querySelectorAll('.opponent-cell').forEach(cell => {
-    cell.addEventListener('click', event => {
-        const player = new Player(false); // Human player
-        const computer = new Player(true); // Computer player
-        handlePlayerAttack(event, player, computer);
+export function initializeGameboardWithRandomShips(gameboard) {
+    const shipLengths = [5, 4, 3, 3, 2]; 
+    shipLengths.forEach(length => {
+        const ship = new Ship(length);
+        gameboard.placeShipRandomly(ship);
     });
-});
+}
+
+const player1 = new Player(false);
+const player2 = new Player(true);
+
+// Ensure only one player has the turn initially
+player1.playerTurn = true;
+player2.playerTurn = false;
+
+// Set opponents
+player1.opponent = player2;
+player2.opponent = player1;
+
+const player1Gameboard = new Gameboard();
+const player2Gameboard = new Gameboard();
+
+initializeGameboardWithRandomShips(player1.gameboard);
+initializeGameboardWithRandomShips(player2.gameboard);
+
+render(player1.gameboard, true); // Render player1's board
+render(player2.gameboard, false); // Render player2's board
+
+
+
